63351f03059979cd7dc14110637f667ee89a7a80
Priyanshu Raj <146614570+Priyanshu845438@users.noreply.github.com>
2025-06-06 10:49:14 +0530

    Time: 431 ms (19.88%), Space: 53 MB (13.36%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> result = new HashSet<>();
+        int n = nums.length;
+        Arrays.sort(nums);  // Sort to help avoid duplicates easily
+
+        for (int i = 0; i < n - 2; i++) {
+            // Avoid duplicates for the first number
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int target = -nums[i];
+            Set<Integer> seen = new HashSet<>();
+
+            for (int j = i + 1; j < n; j++) {
+                int complement = target - nums[j];
+                if (seen.contains(complement)) {
+                    List<Integer> triplet = Arrays.asList(nums[i], complement, nums[j]);
+                    result.add(triplet);
+                }
+                seen.add(nums[j]);
+            }
+        }
+
+        return new ArrayList<>(result);
+    }
+}
